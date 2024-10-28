c92bae7240c7af4981bf723511a5c56a9938c3e9
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-10-27 19:36:57 -0700

    Implement block_to_block_type function and unit tests

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import block_to_block_type, markdown_to_blocks, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,22 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING.value)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE.value)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE.value)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST.value)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST.value)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST.value)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
 
 
 if __name__ == "__main__":
