7139f9caa1f0125649e5f58d604639b8c8d53e9c
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-10-30 17:14:18 -0700

    Add copy_static.py to recursively copy static files to public directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+
+from copy_static import copy_files_recursive
+
+DIR_PATH_STATIC = "./static"
+DIR_PATH_PUBLIC = "./public"
 
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
-    print(text_node)
+    if os.path.exists(DIR_PATH_PUBLIC):
+        print("Deleting public directory...")
+        shutil.rmtree(DIR_PATH_PUBLIC)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(DIR_PATH_STATIC, DIR_PATH_PUBLIC)
 
 
 main()
